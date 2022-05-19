4b4c14eeaa539c4f777ab12d7c97f71e7d3a4e62
BL-Jotiram <bandgar09@gmail.com>
2022-05-19 10:50:17 +0530

    Added code to roll dice

--- src/com/bridgelabz/snakeladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -3,5 +3,6 @@ package com.bridgelabz.snakeladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int playerPosition = 0;
+        double dice = (Math.random() * 10) % 6;
     }
 }
